87f3e7e24513e39b8e34d00a0edb516d01e0890f
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2023-09-29 16:55:29 -0300

    :sparkles: feat: layout view

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,14 +1,4 @@
-const HomeModel = require('../models/HomeModel');
-
-exports.paginaInicial = (req, res) => {
-    res.render('index', {
-        titulo: 'Este será o <span style="color:red">título</span> da página', 
-        numeros: [0,1,2,3,4,5,6,7,8,9]
-    });
+exports.index = (request, response) => {
+    response.render('index');
     return;
 };
-
-exports.trataPost = (req, res, next) => {
-    res.send(req.body);
-    return;
-}
