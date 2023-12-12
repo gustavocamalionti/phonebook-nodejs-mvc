dc611feded70ee9814cdeabdb7d9ca7b51959cbf
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2023-12-12 10:35:38 -0300

    :sparkles: feat: login users

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,6 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (request, response) => {
+    // console.log(request.session.user);
+    if (request.session.user) return response.render('login-logado');
     response.render('login');
 }
 
@@ -28,4 +30,46 @@ exports.register = async (request, response) => {
         return res.render('404');
 
     }
+}
+
+exports.login = async (request, response) => {
+    try {
+        const login = new Login(request.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            request.flash('errors', login.errors);
+            //Salvar a sessão
+            request.session.save(function () {
+                //Agora que a sessão já está salva, está seguro para voltar de onde o formulário veio
+                return response.redirect('back');
+            })
+            return;
+        }
+
+        if (login.errors.length > 0) {
+            request.flash('errors', login.errors);
+            //Salvar a sessão
+            request.session.save(function () {
+                //Agora que a sessão já está salva, está seguro para voltar de onde o formulário veio
+                return response.redirect('back');
+            })
+            return;
+        }
+
+        request.flash('success', 'Você entrou no sistema.');
+        request.session.user = login.user;
+        request.session.save(function() {
+            return response.redirect('back');
+        })
+    } catch (error) {
+        console.log(error);
+        return res.render('404');
+
+    }
+}
+
+exports.logout = async (request, response) => {
+  request.session.destroy();
+  response.redirect('/');
 }
